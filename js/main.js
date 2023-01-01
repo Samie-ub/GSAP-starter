4320a897db53c6b1c9f6cb032d838fa161f3a841
samie-ub <xxxsamie39@gmail.com>
2023-01-02 02:57:25 +0500

    01/jan/2023 02:57 AM

--- js/main.js
+++ js/main.js
@@ -20,4 +20,22 @@ gsap.from(".logog, .new",{duration:5, opacity:0, scale: 0.3, ease:'back'})
 // creating random animation circle method one
 // gsap.from(".circle",{duration:1, opacity:0, y:()=>Math.random()*400-200, stagger:0.125})
 // creating random animation circle method two
-gsap.from(".circle",{duration:1, opacity:0, y:()=>Math.random()*400-200, stagger:0.125})
+gsap.from(".circle",{duration:1, opacity:0, y:"random(-200, 200)", stagger:0.125})
+
+// Button events
+
+let tween = gsap.to(".circle", {
+    duration: 2, 
+    stagger:0.1,
+    yPercent:"random(-200, 200)",
+    xPercent: "random(-400, 400)", // offset by the width of the box
+    rotation: 60, 
+    ease: "none", 
+    paused: true
+  });
+
+  document.querySelector("#play").onclick = () => tween.play();
+  document.querySelector("#pause").onclick = () => tween.pause();
+  document.querySelector("#resume").onclick = () => tween.resume();
+  document.querySelector("#reverse").onclick = () => tween.reverse();
+  document.querySelector("#restart").onclick = () => tween.restart();
